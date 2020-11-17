c9f484536ab36d1ac775a656e98d467b9722a5c4
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-17 17:47:48 +0100

    -

--- js/hand-in-2.js
+++ js/hand-in-2.js
@@ -1,1 +1,8 @@
-console.log("Hello Hand-In 2 World!");
+AFRAME.registerComponent('test-component', {
+    init: function()
+    {
+        this.el.addEventListener('click', function(){
+            console.log("Hello Hand-In 2 World!");
+        })
+    }
+}); 
