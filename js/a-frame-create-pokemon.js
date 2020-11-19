002c99e84a32d25c0c71690a7dbe12f322e3b9d1
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-20 00:12:38 +0100

    Create a-frame-create-pokemon.js

---
+++ js/a-frame-create-pokemon.js
@@ -0,0 +1,5 @@
+/*
+ * So my HTML was getting reaaaally long, and the pokemon are a bit complex by now. 
+ * I want to create a component that can make the Pokémons instead of them being hardcoded to the plane. 
+ * Wish me luck!
+ */
