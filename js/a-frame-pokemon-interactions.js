91aca944ce299c1ee141c658b455ae8dda8514cd
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-19 23:15:49 +0100

    Update a-frame-pokemon-interactions.js

--- js/a-frame-pokemon-interactions.js
+++ js/a-frame-pokemon-interactions.js
@@ -6,15 +6,16 @@ AFRAME.registerComponent('pokemon', {
     },
 
     tick: function () {		
-    this.el.addEventListener('collide', function (e) 
-    {
-        let pokemon = e.detail.target.el;  // Original entity.
-        let collidedObj = e.detail.body.el; // Other entity, which playerEl touched.
+      this.el.addEventListener('collide', function (e) 
+      {
+          let pokemon = e.detail.target.el;  // Original entity.
+          let collidedObj = e.detail.body.el; // Other entity, which playerEl touched.
 
-        if(collidedObj.classList.contains("pokeball"))
-        {      
-            collidedObj.setAttribute('sound', {src: 'sounds/sfx/PokeballOpening.mp3', autoplay: 'true', loop: 'false'});
-        }
-		});
+          if(collidedObj.classList.contains("pokeball"))
+          {      
+              collidedObj.setAttribute('sound', {src: 'sounds/sfx/PokeballOpening.mp3', autoplay: 'true', loop: 'false'});          
+              pokemon.setAttribute('material', {color: '#FFFFFF'});
+          }
+      });
 	}
 });
