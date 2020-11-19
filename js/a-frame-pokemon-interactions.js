f2e169312396e020b28dc290c5299832727b9d21
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-19 23:55:03 +0100

    Update a-frame-pokemon-interactions.js

--- js/a-frame-pokemon-interactions.js
+++ js/a-frame-pokemon-interactions.js
@@ -1,22 +1,22 @@
 AFRAME.registerComponent('pokemon', {   
-	init: function () {		
-		this.el.addEventListener('click', function () {
-            console.log(this.el + " was clicked!");
+	  init: function () {		
+		  this.el.addEventListener('click', function () {
+        console.log(this.el + " was clicked!");
 	  	});
     },
 
     tick: function () {		
-      this.el.addEventListener('collide', function (e) 
-      {
-          let pokemon = e.detail.target.el;  // Original entity.
-          let collidedObj = e.detail.body.el; // Other entity, which playerEl touched.
+      this.el.addEventListener('collide', this.catchPokemon)
+    },
 
-          if(collidedObj.classList.contains("pokeball"))
-          {      
-              collidedObj.setAttribute('sound', {src: 'sounds/sfx/PokeballOpening.mp3', autoplay: 'true', loop: 'false'});          
-              pokemon.setAttribute('animation', {property: 'scale', to: '0 0 0', dur: '3000', easing: 'linear'});  
-              pokemon.components.material.material.color = "#000000"
-          }
-      });
-	}
+    catchPokemon: function (e) {		
+      let pokemon = e.detail.target.el;  // Original entity.
+      let collidedObj = e.detail.body.el; // Other entity, which playerEl touched.
+
+      if(collidedObj.classList.contains("pokeball"))
+      {      
+        collidedObj.setAttribute('sound', {src: 'sounds/sfx/PokeballOpening.mp3', autoplay: 'true', loop: 'false'});          
+        pokemon.setAttribute('animation', {property: 'scale', to: '0 0 0', dur: '2500', easing: 'linear'});  
+      }
+    }
 });
