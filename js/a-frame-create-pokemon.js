72e7194829c7ec1045e4244e9f95ccc18a2abc74
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-20 00:14:47 +0100

    Update a-frame-create-pokemon.js

--- js/a-frame-create-pokemon.js
+++ js/a-frame-create-pokemon.js
@@ -3,3 +3,19 @@
  * I want to create a component that can make the Pokémons instead of them being hardcoded to the plane. 
  * Wish me luck!
  */
+
+/* Meant to spawn wild pokemon */
+AFRAME.registerComponent('pokemon-generator', {
+    schema: {},
+	init: function () {
+
+    },
+});
+
+/* Meant for objects that can hold a select amount of pokemon and save them (pokeballs) */
+AFRAME.registerComponent('pokemon-container', {
+    schema: {},
+	init: function () {
+
+    },
+});
