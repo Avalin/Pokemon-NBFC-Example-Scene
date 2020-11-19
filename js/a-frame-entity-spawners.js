a67ae01fd38c937dd8133a3fc2bc839d5531717b
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-19 20:35:21 +0100

    Pokeball limit

--- js/a-frame-entity-spawners.js
+++ js/a-frame-entity-spawners.js
@@ -33,32 +33,39 @@ AFRAME.registerComponent('pokeball-spawner', {
     },
     
     instantiatePokeball: function () {
-        //Setup the spawn-point for the new pokeball to be in the basket in the scene
-        let spawnPoint = document.querySelector('#pokeball_spawn_point');
-        let scene = document.querySelector('a-scene');
-        let position = new THREE.Vector3(0, 5, 0);
-        let direction = new THREE.Vector3();
-        let target = new THREE.Vector3();
-        
-        spawnPoint.object3D.getWorldPosition(position);
-        target.add(position).add(direction);
+        let pokeballsInSceneAmount = document.getElementsByClassName('pokeball').length + 1;
 
-        //Create the pokeball container, with collider and grabbable sphere
-        let pokeballContainer = document.createElement('a-sphere');
-        pokeballContainer.setAttribute('id', 'PLEASELOVEME');
-        pokeballContainer.setAttribute('grabbable');
-        pokeballContainer.setAttribute('dynamic-body', {mass: '0.08', linearDamping: '0.05', angularDamping: '0.3', shape: 'sphere', sphereRadius: '0.125'});
-        pokeballContainer.setAttribute('material', {color: '#FF00FF', side: 'double'});
-        pokeballContainer.setAttribute('radius', 5);
-        pokeballContainer.setAttribute('scale', '0.02 0.02 0.02');
-        pokeballContainer.setAttribute('position', target);
-         
-        //Creates the visible model
-        let pokeballMesh = document.createElement('a-entity');
-        pokeballContainer.appendChild(pokeballMesh);
-        pokeballMesh.setAttribute('position', '0 -6 0');
-        pokeballMesh.setAttribute('gltf-model', '3dmodels/Objects/Pokeball/pokeball.glb');
-        pokeballMesh.setAttribute('scale', '600 600 600');
-        scene.appendChild(pokeballContainer);
+        if(pokeballsInSceneAmount <= 3)
+        {
+            //Setup the spawn-point for the new pokeball to be in the basket in the scene
+            let spawnPoint = document.querySelector('#pokeball_spawn_point');
+            let scene = document.querySelector('a-scene');
+            let position = new THREE.Vector3(0, 5, 0);
+            let direction = new THREE.Vector3();
+            let target = new THREE.Vector3();
+            
+            spawnPoint.object3D.getWorldPosition(position);
+            target.add(position).add(direction);
+
+            //Create the pokeball container, with collider and grabbable sphere
+            let pokeballContainer = document.createElement('a-sphere');
+            pokeballContainer.setAttribute('id', 'PLEASELOVEME');
+            pokeballContainer.classList.add('interactable')
+            pokeballContainer.classList.add('pokeball')
+            pokeballContainer.setAttribute('grabbable', '');
+            pokeballContainer.setAttribute('dynamic-body', {mass: '0.08', linearDamping: '0.05', angularDamping: '0.3', shape: 'sphere', sphereRadius: '0.125'});
+            pokeballContainer.setAttribute('material', {color: '#FF00FF', side: 'double'});
+            pokeballContainer.setAttribute('radius', 5);
+            pokeballContainer.setAttribute('scale', '0.02 0.02 0.02');
+            pokeballContainer.setAttribute('position', target);
+            
+            //Creates the visible model
+            let pokeballMesh = document.createElement('a-entity');
+            pokeballContainer.appendChild(pokeballMesh);
+            pokeballMesh.setAttribute('position', '0 -6 0');
+            pokeballMesh.setAttribute('gltf-model', '3dmodels/Objects/Pokeball/pokeball.glb');
+            pokeballMesh.setAttribute('scale', '600 600 600');
+            scene.appendChild(pokeballContainer);
+        }
     }
 });
