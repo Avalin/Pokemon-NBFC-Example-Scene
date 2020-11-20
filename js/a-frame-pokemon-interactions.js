fe760f27689fa83f5dee167cc13e83f6de1c4ceb
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-20 13:15:01 +0100

    Update a-frame-pokemon-interactions.js

--- js/a-frame-pokemon-interactions.js
+++ js/a-frame-pokemon-interactions.js
@@ -20,19 +20,15 @@ AFRAME.registerComponent('pokemon', {
       pokemon.addEventListener('collide', this.catchPokemon)
     },
 
-    tick: function () {		
-
-    },
-
     catchPokemon: function (e) {		
-      let pokemon = e.detail.target.el;  // Original entity.
-      let otherCollider = e.detail.body.el; // Other entity, which playerEl touched.
-      let pokemonMesh = document.getElementById(pokemon.parentNode.id + "_mesh");
+        let pokemon = e.detail.target.el;  // Original entity.
+        let otherCollider = e.detail.body.el; // Other entity, which playerEl touched.
+        let pokemonMesh = document.getElementById(pokemon.parentNode.id + "_mesh");
 
-      if(otherCollider.classList.contains("pokeball"))
-      {      
-        otherCollider.setAttribute('sound', {src: 'sounds/sfx/PokeballOpening.mp3', autoplay: 'true', loop: 'false'});          
-        pokemonMesh.setAttribute('animation', {property: 'scale', to: '0 0 0', dur: '2500', easing: 'linear'});  
-      }
+        if(otherCollider.classList.contains("pokeball"))
+        {      
+          otherCollider.setAttribute('sound', {src: 'sounds/sfx/PokeballOpening.mp3', autoplay: 'true', loop: 'false'});          
+          pokemonMesh.setAttribute('animation', {property: 'scale', to: '0 0 0', dur: '2500', easing: 'linear'});  
+        }
     },
 });
