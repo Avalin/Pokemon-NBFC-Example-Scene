31e60320f4f62503447ea674493a224bd44aa2f1
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-20 16:09:39 +0100

    -

--- js/a-frame-light-modifiers.js
+++ js/a-frame-light-modifiers.js
@@ -17,17 +17,20 @@ AFRAME.registerComponent('light-modifier', {
     },
     init: function()
     {
+		let lightbulb =  this.el;
 		let lightObj =  this.data.target_light;
         this.el.addEventListener('click', function(){
             //To do
             let lightEl = lightObj.getAttribute('light');
             if(lightEl.intensity > 0)
             {
-                lightObj.setAttribute('light', {color: '#ACC', intensity: 0});
+                lightbulb.setAttribute('color', '#666');
+                lightObj.setAttribute('light', {color: '#000', intensity: 0});
             }
             else
             { 
-                lightObj.setAttribute('light', {color: '#ACC', intensity: 1});
+                lightbulb.setAttribute('color', '#FFF');
+                lightObj.setAttribute('light', {color: '#FF9900', intensity: 1});
             }
         })
     }
