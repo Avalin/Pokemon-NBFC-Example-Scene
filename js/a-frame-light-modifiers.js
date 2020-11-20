e6d33fdf0ee181b25bcccaaec4c2832542ff8626
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-20 05:35:11 +0100

    -

--- js/a-frame-light-modifiers.js
+++ js/a-frame-light-modifiers.js
@@ -1,7 +1,7 @@
 /*
-* Consider this component as for hand-in 1: 
+* Consider this component as for hand-in 1, last assignment: 
 * "Setup handlers for animation and show basic coded animations on your models, lights etc.""
-* May be used in hand-in 2 as well.
+* May be used in hand-in 2 as well if I feel like it.
 */
 
     /*
